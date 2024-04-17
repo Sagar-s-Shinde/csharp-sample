867f8eabee1405e7e6380610b61673946a091d6e
Sagar-s-Shinde <sagar.it01@gmail.com>
2024-04-17 15:40:51 +0530

    added sample api

---
+++ sampleapi/sampleapi/Controllers/wellcomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace sampleapi.Controllers
+{
+    [Route("wellcome")]
+    [ApiController]
+    public class wellcomeController : ControllerBase
+    {
+        [Route("Message")]
+        public IActionResult Get([FromQuery] string str="guest")
+        {
+            return Content(("Welcome to Microservices : " + str));
+        }
+        //// GET: api/<wellcomeController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/<wellcomeController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<wellcomeController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<wellcomeController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<wellcomeController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
